9caae7c9e78e5f866fb9adfbf9af79dbbdd4956d
Sebdiez10 <62372530+Sebdiez10@users.noreply.github.com>
2020-11-17 16:21:16 +1100

    CustomerService & CustomerDao

--- JDBC-Project-Group-B/com/dao/CustomerDao.java
+++ JDBC-Project-Group-B/com/dao/CustomerDao.java
@@ -1,226 +1,94 @@
 package com.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
+import com.bean.Customer;
 import com.util.DBUtil;
 
 public class CustomerDao {
 
-	public CustomerDao() {
-		super();
-	}
-
-	public boolean updateCustomerName(int customerId, String name) {
-
-		boolean result = false;
-
-		try {
-
-			Connection cn = DBUtil.createConnection();
-			PreparedStatement ps = cn.prepareStatement("UPDATE CUSTOMERS SET name=? WHERE id=?");
-
-			ps.setString(1, name);
-			ps.setInt(2, customerId);
-
-			int n = ps.executeUpdate();
-			result = n > 0;
-
-			// Close all the objects in the reverse order of creation
-			DBUtil.closeAllConnection(cn, ps, null);
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		return result;
-	}
-
-	public boolean updateCustomerAddress(int customerId, String address) {
-
-		boolean result = false;
-
-		try {
-
-			Connection cn = DBUtil.createConnection();
-			PreparedStatement ps = cn.prepareStatement("UPDATE CUSTOMERS SET address=? WHERE id=?");
-
-			ps.setString(1, address);
-			ps.setInt(2, customerId);
-
-			int n = ps.executeUpdate();
-			result = n > 0;
-
-			// Close all the objects in the reverse order of creation
-			DBUtil.closeAllConnection(cn, ps, null);
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		return result;
-	}
-
-	public boolean updateCustomerPhone(int customerId, String phone) {
-
-		boolean result = false;
-
-		try {
-
-			Connection cn = DBUtil.createConnection();
-			PreparedStatement ps = cn.prepareStatement("UPDATE CUSTOMERS SET phone=? WHERE id=?");
-
-			ps.setString(1, phone);
-			ps.setInt(2, customerId);
-
-			int n = ps.executeUpdate();
-			result = n > 0;
-
-			// Close all the objects in the reverse order of creation
-			DBUtil.closeAllConnection(cn, ps, null);
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		return result;
-	}
-
-	public boolean updateCustomerGender(int customerId, String gender) {
-
-		boolean result = false;
-
-		try {
-
-			Connection cn = DBUtil.createConnection();
-			PreparedStatement ps = cn.prepareStatement("UPDATE CUSTOMERS SET gender=? WHERE id=?");
-
-			ps.setString(1, gender);
-			ps.setInt(2, customerId);
-
-			int n = ps.executeUpdate();
-			result = n > 0;
-
-			// Close all the objects in the reverse order of creation
-			DBUtil.closeAllConnection(cn, ps, null);
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		return result;
-	}
-
-	public boolean updateCustomerEmail(int customerId, String email) {
-
-		boolean result = false;
-
-		try {
-
-			Connection cn = DBUtil.createConnection();
-			PreparedStatement ps = cn.prepareStatement("UPDATE CUSTOMERS SET gender=? WHERE id=?");
-
-			ps.setString(1, email);
-			ps.setInt(2, customerId);
-
-			int n = ps.executeUpdate();
-			result = n > 0;
-
-			// Close all the objects in the reverse order of creation
-			DBUtil.closeAllConnection(cn, ps, null);
-
-		} catch (SQLException e) {
-
-			System.out.println(e.getMessage());
-
-		}
-
-		return result;
-	}
-
-	public boolean addCustomer(Customer c) {
-
-		boolean result = false;
-
-		try {
-			// Create a Connection object
-			Connection cn = DBUtil.createConnection();
-
-			// Create a PreparedStatement object using the Connection
-			PreparedStatement ps = cn.prepareStatement("INSERT INTO CUSTOMERS VALUES(?,?,?,?,?,?)");
-
-			ps.setInt(1, c.getId());
-			ps.setString(2, c.getName());
-			ps.setString(3, String.valueOf(c.getGender()));
-			ps.setString(4, c.getEmail());
-			ps.setString(5, c.getPhone());
-			ps.setString(6, c.getAddress());
-
-			// Execute query and store the result.
-			int n = ps.executeUpdate();
-
-			// Check if the query is a success or fails.
-			if (n > 0) {
-				result = true;
-			}
-
-			// Close all the objects in the reverse order of its creation.
-			DBUtil.closeAllConnection(cn, ps, null);
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		return result;
-	}
-
-	public Customer viewCustomerDetails(int cid) {
-		Customer c = null;
-		try {
-			// Create a Connection object
-			Connection cn = DBUtil.createConnection();
-
-			// Create a PreparedStatement object using the Connection
-			PreparedStatement ps = cn.prepareStatement("SELECT * FROM CUSTOMERS WHERE ID=?");
-
-			ps.setLong(1, cid);
-
-			// Execute the query and store the result.
-			ResultSet rs = ps.executeQuery();
-
-			// Iterate the result set and extract the information.
-			if (rs != null) {
-				while (rs.next()) {
-					int id = rs.getInt("ID");
-					String name = rs.getString("NAME");
-					char gender = rs.getString("GENDER").charAt(0);
-					String email = rs.getString("EMAIL");
-					String phone = rs.getString("PHONE");
-					String address = rs.getString("ADDRESS");
-					c = new Customer(id, name, gender, email, phone, address);
+		
+		public boolean addCustomer(Customer c) {
+
+			boolean result = false;
+
+			try {
+				//Create a Connection object
+				Connection cn = DBUtil.createConnection();
+
+				//Create a PreparedStatement object using the Connection
+				PreparedStatement ps = cn
+						.prepareStatement("INSERT INTO CUSTOMERS VALUES(?,?,?,?,?,?)");
+
+				ps.setInt(1, c.getId());
+				ps.setString(2, c.getName());
+				ps.setString(3, String.valueOf(c.getGender()));
+				ps.setString(4, c.getEmail());
+				ps.setString(5, c.getPhone());
+				ps.setString(6, c.getAddress());
+				
+
+				//Execute query and store the result.
+				int n = ps.executeUpdate();
+
+				//Check if the query is a success or fails.
+				if (n > 0) {
+					result = true;
 				}
+
+				//Close all the objects in the reverse order of its creation.
+				DBUtil.closeAllConnection(cn, ps, null);
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
 			}
-
-			// Close all the objects in the reverse order of its creation.
-			DBUtil.closeAllConnection(cn, ps, rs);
-
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			return result;
 		}
-		return c;
+		
+		
+		public Customer viewCustomerDetails(int cid) {
+			Customer c = null;
+			try {
+				//Create a Connection object
+				Connection cn = DBUtil.createConnection();
 
+				//Create a PreparedStatement object using the Connection
+				PreparedStatement ps = cn
+						.prepareStatement("SELECT * FROM CUSTOMERS WHERE ID=?");
+
+				ps.setLong(1, cid);
+				
+				//Execute the query and store the result.
+				ResultSet rs = ps.executeQuery();
+				
+				//Iterate the result set and extract the information.
+				if (rs != null) {
+					while (rs.next()) {
+						int id = rs.getInt("ID");
+						String name = rs.getString("NAME");
+						char gender = rs.getString("GENDER").charAt(0);
+						String email = rs.getString("EMAIL");
+						String phone = rs.getString("PHONE");
+						String address = rs.getString("ADDRESS");
+						c = new Customer(id, name, gender, email, phone, address);
+					}
+				}
+				
+				//Close all the objects in the reverse order of its creation.
+				DBUtil.closeAllConnection(cn, ps, rs);
+
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+			return c;
+				
+			}
+			
+			
+			
 	}
 
-}
--- JDBC-Project-Group-B/com/service/CustomerService.java
+++ JDBC-Project-Group-B/com/service/CustomerService.java
@@ -1,5 +1,25 @@
 package com.service;
 
-public class CustomerService {
+import com.bean.Customer;
+import com.dao.CustomerDao;
 
+
+
+public class CustomerService {
+	
+	
+	//Add customer
+	public boolean addCustomer(Customer c) {
+		CustomerDao dao=new CustomerDao();
+		return dao.addCustomer(c);
+	}
+	
+	//View customer details by Id
+	public Customer viewCustomerDetails(int cid) {
+		CustomerDao dao=new CustomerDao();
+		return dao.viewCustomerDetails(cid);
+	}
+
+	
+	
 }
