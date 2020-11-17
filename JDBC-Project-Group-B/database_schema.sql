c2b362b7aa7746727a5768a016b2c989fae236a1
Winton Nathan-Roberts <wintonnathan-roberts@MoltenReact.local>
2020-11-17 18:12:55 +1100

    updated Schema

--- JDBC-Project-Group-B/database_schema.sql
+++ JDBC-Project-Group-B/database_schema.sql
@@ -19,6 +19,18 @@ CREATE TABLE CUSTOMERS(
 	CONSTRAINT ck_GENDER
 		CHECK (gender in ('M','F','O'))
 );
+/* id Auto Increment */
+CREATE SEQUENCE cust_id_seq START WITH 1;
+/* Must run the following as "One Statement"*/
+CREATE OR REPLACE TRIGGER cust_id_trig
+  BEFORE INSERT ON CUSTOMERS
+  FOR EACH ROW
+BEGIN
+  SELECT cust_id_seq.nextval
+  INTO :new.id
+  FROM dual;
+END;
+
 
 /* Login Details for Bank Clerks */
 
@@ -28,27 +40,64 @@ CREATE TABLE CLERKS(
 );
 
 
+
 /* Accounts */
 
 CREATE TABLE BANK_ACCOUNTS(
 	acc_id NUMBER PRIMARY KEY,
 	customer_id NUMBER NOT NULL REFERENCES CUSTOMERS(id),
 	balance NUMBER DEFAULT 0,
-	acc_type VARCHAR(10) NOT NULL,
+	acc_type VARCHAR(10) DEFAULT 'REGULAR',
 	CONSTRAINT ck_BANK_TYPE
 		CHECK (acc_type in ('REGULAR','CREDIT'))
 );
 
-/* Transactions */
+/* acc_id Auto Increment */
+CREATE SEQUENCE acc_id_seq START WITH 1;
+/* Must run the following as "One Statement"*/
+CREATE OR REPLACE TRIGGER acc_id_trig
+  BEFORE INSERT ON BANK_ACCOUNTS
+  FOR EACH ROW
+BEGIN
+  SELECT acc_id_seq.nextval
+  INTO :new.acc_id
+  FROM dual;
+END;
 
+
+
+/* Transactions */
 CREATE TABLE TRANSACTIONS(
 	txn_id NUMBER PRIMARY KEY,
 	acc_id NUMBER NOT NULL REFERENCES BANK_ACCOUNTS(acc_id),
-	ts DATE NOT NULL,
-	txn_type VARCHAR(10) NOT NULL,
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	txn_type VARCHAR(10) DEFAULT 'REGULAR',
 	amount NUMBER NOT NULL,
 	CONSTRAINT ck_TXN_TYPE
 		CHECK (txn_type in ('REGULAR','CREDIT','OVERDRAFT'))
 );
+
+/* Txn Auto Increment */
+CREATE SEQUENCE txn_id_seq START WITH 1;
+/* Must run the following as "One Statement"*/
+CREATE OR REPLACE TRIGGER txn_id_trig
+  BEFORE INSERT ON TRANSACTIONS
+  FOR EACH ROW
+BEGIN
+  SELECT txn_id_seq.nextval
+  INTO :new.txn_id
+  FROM dual;
+END;
+
+
+
+/* Test */
+insert into CUSTOMERS(name, gender) values('mark', 'M');
+insert into BANK_ACCOUNTS(customer_id) values(1);
+insert into TRANSACTIONS(acc_id,amount) values (1,333);
+select * from TRANSACTIONS;
+select * from BANK_ACCOUNTS;
+select * from CUSTOMERS;
+
 
 ROLLBACK TO makeTables;
