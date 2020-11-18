9fca53bedcb501e14e90a5ec2e93a24ad8b2a5f1
AshAbay <arsha.abayasingam99@gmail.com>
2020-11-18 11:21:24 +1100

    Deposit and withdraw dao done.

--- JDBC-Project-Group-B/com/service/TransactionService.java
+++ JDBC-Project-Group-B/com/service/TransactionService.java
@@ -2,4 +2,30 @@ package com.service;
 
 public class TransactionService {
 
+//    public boolean depositMoney(int acc_id, int amount){
+//
+//        boolean transactionResult = createTransaction(acc_id, amount);
+//        if (!transactionResult){
+//            return false;
+//        }
+//        boolean bankUpdate = addMoneyBank(acc_id, amount);
+//        if (!bankUpdate){
+//            return false;
+//        }
+//        return true;
+//    }
+//    public boolean withdrawMoney(int acc_id, int amount){
+//
+//        amount = -1*amount;
+//        boolean transactionResult = createTransaction(acc_id, amount);
+//        if (!transactionResult){
+//            return false;
+//        }
+//        boolean bankUpdate = addMoneyBank(acc_id, amount);
+//        if (!bankUpdate){
+//            return false;
+//        }
+//        return true;
+//    }
+
 }
