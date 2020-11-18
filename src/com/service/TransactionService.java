364e619bcb0ac30ac63138c10cdb82df89f5f2a7
AshAbay <arsha.abayasingam99@gmail.com>
2020-11-18 13:58:39 +1100

    Working on updating bank accounts

--- src/com/service/TransactionService.java
+++ src/com/service/TransactionService.java
@@ -11,31 +11,6 @@ public class TransactionService {
 		return dao.getTransactionsByBankAccount(accountId);
 	}
 
-//    public boolean depositMoney(int acc_id, int amount){
-//
-//        boolean transactionResult = createTransaction(acc_id, amount);
-//        if (!transactionResult){
-//            return false;
-//        }
-//        boolean bankUpdate = addMoneyBank(acc_id, amount);
-//        if (!bankUpdate){
-//            return false;
-//        }
-//        return true;
-//    }
-//    public boolean withdrawMoney(int acc_id, int amount){
-//
-//        amount = -1*amount;
-//        boolean transactionResult = createTransaction(acc_id, amount);
-//        if (!transactionResult){
-//            return false;
-//        }
-//        boolean bankUpdate = addMoneyBank(acc_id, amount);
-//        if (!bankUpdate){
-//            return false;
-//        }
-//        return true;
-//    }
 	public boolean depositMoney(int acc_id, int amount) {
 		TransactionDao transactionDao = new TransactionDao();
 		boolean transactionResult = transactionDao.createTransaction(acc_id, amount);
