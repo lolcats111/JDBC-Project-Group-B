686e5adf0755888e93a6dc4d8b8349d6a730a561
Lachlan Hamilton <lachlanbhamilton@gmail.com>
2020-11-19 13:47:53 +1100

    addCustomer and output handling

--- src/com/main/CommandLineInterface.java
+++ src/com/main/CommandLineInterface.java
@@ -3,17 +3,28 @@ package com.main;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
-import com.service.CreateAccountService;
-import com.service.LoginService;
+import com.bean.*;
+import com.service.*;
 
 public class CommandLineInterface {
 	
-	static int lineLength = 40;
-	static int screenHeight = 20;
-	static int indent = 5;
+	static final int lineLength = 40;
+	static final int screenHeight = 20;
+	static final int indent = 5;
 	
-	static int noOption = 0;
-	static int quitOption = 11;
+	static final int noOption = 0;
+	static final int addCustomerOption = 1;
+	static final int listCustomersOption = 2;
+	static final int viewCustomerOption = 3;
+	static final int editCustomerOption = 4;
+	static final int deleteCustomerOption = 5;
+	static final int openAccountOption = 6;
+	static final int closeAccountOption = 7;
+	static final int depositAmountOption = 8;
+	static final int withdrawAmountOption = 9;
+	static final int viewTransactionsOption = 10;
+	static final int addClerkAccountOption = 11;
+	static final int quitOption = 12;
 
 	/**
 	 * @param args
@@ -22,9 +33,10 @@ public class CommandLineInterface {
 		
 		boolean running = true;
 		boolean loggedIn = false;
+		String output = "";
 		int option = 0;
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		// Main command loop.
 		while(running) {
@@ -35,20 +47,22 @@ public class CommandLineInterface {
 				
 				loggedIn = runLogin();
 				
-			} else {
+			} else if (output.equals("")) {
 				
 				if (option == noOption) {
 					option = runOptionSelect();
 				} else if (option == quitOption) {
 					running = false;
 				} else {
-					
-					// Option handler goes here.
-					System.out.println(option);
-					break;
+					output = optionHandler(option);
+					option = noOption;
 				}
-				
-				
+			} else {
+				System.out.println();
+				System.out.println(output);
+				System.out.println(header("Press ENTER to continue."));
+				sc.nextLine();
+				output = "";
 			}
 			
 			System.out.println(clearScreen());
@@ -86,17 +100,18 @@ public class CommandLineInterface {
 		
 		System.out.println(header("Welcome User."));
 		
-		System.out.println(option(1, "Add Customer"));
-		System.out.println(option(2, "List Customers"));
-		System.out.println(option(3, "View Customer"));
-		System.out.println(option(4, "Edit Customer"));
-		System.out.println(option(5, "Delete Customer"));
-		System.out.println(option(6, "Open Account"));
-		System.out.println(option(7, "Close Account"));
-		System.out.println(option(8, "Deposit Amount"));
-		System.out.println(option(9, "Withdraw Amount"));
-		System.out.println(option(10, "View Trasactions"));
-		System.out.println(option(11, "Quit"));
+		System.out.println(option(addCustomerOption, "Add Customer"));
+		System.out.println(option(listCustomersOption, "List Customers"));
+		System.out.println(option(viewCustomerOption, "View Customer"));
+		System.out.println(option(editCustomerOption, "Edit Customer"));
+		System.out.println(option(deleteCustomerOption, "Delete Customer"));
+		System.out.println(option(openAccountOption, "Open Account"));
+		System.out.println(option(closeAccountOption, "Close Account"));
+		System.out.println(option(depositAmountOption, "Deposit Amount"));
+		System.out.println(option(withdrawAmountOption, "Withdraw Amount"));
+		System.out.println(option(viewTransactionsOption, "View Trasactions"));
+		System.out.println(option(addClerkAccountOption, "Add Clerk Account"));
+		System.out.println(option(quitOption, "Quit"));
 		
 		System.out.println();
 		System.out.print(field("Select your option", 15));
@@ -105,6 +120,69 @@ public class CommandLineInterface {
 		return option;
 		
 	}
+	
+	static String optionHandler(int option) {
+		
+		String output = "";
+		
+		switch (option) {
+		  case addCustomerOption:
+		    output = runAddCustomer();
+		    break;
+		  case listCustomersOption:
+		    output = runListCustomers();
+		    break;
+		}
+		
+		
+		return output;
+		
+	}
+	
+	static String runAddCustomer() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(header("Customer Registration"));
+		
+		System.out.print(field("Name", 15));
+		String name = sc.nextLine();
+		System.out.print(field("Gender", 15));
+		char gender = sc.nextLine().charAt(0);
+		System.out.print(field("Email", 15));
+		String email = sc.nextLine();
+		System.out.print(field("Phone", 15));
+		String phone = sc.nextLine();
+		System.out.print(field("Address", 15));
+		String address = sc.nextLine();
+		System.out.print(field("Privileged", 15));
+		String isPrivileged = sc.nextLine();
+		
+		CustomerService cs = new CustomerService();
+		Customer customer = new Customer(name, gender, email, phone, address, isPrivileged);
+		
+		return display(cs.addCustomer(customer));
+	}
+
+	static String runListCustomers() {
+		return "";
+	}
+	
+	static String display (Customer customer) {
+		
+		return field("Id", 15) + customer.getId() + "\n"
+				+ field("Name", 15) + customer.getName() + "\n"
+				+ field("Gender", 15) + customer.getGender() + "\n"
+				+ field("Email", 15) + customer.getEmail() + "\n"
+				+ field("Phone", 15) + customer.getPhone() + "\n"
+				+ field("Address", 15) + customer.getAddress() + "\n"
+				+ field("Privileged", 15) + customer.getIsPrivileged();
+	}
+	
+	static String display (BankAccount account) {
+		return "";
+	}
+	
 	
 	static String header (String text) {
 		return ("\n" + center(text) + "\n");
